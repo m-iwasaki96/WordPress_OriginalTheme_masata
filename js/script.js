ff4fddfa8ea49bb5868be0e09ecea4f8a629bb00
m-iwasaki96 <iwasaki.ocu96@gmail.com>
2023-10-15 05:53:25 +0900

    JavaScriptファイル微調整

--- js/script.js
+++ js/script.js
@@ -52,18 +52,16 @@ jQuery(function ($) {
   });
 
   // ページ内スクロール
-  $(function () {
-    // ヘッダーの高さ取得
-    const headerHeight = $(".js-header").height();
-    $('a[href^="#"]').click(function () {
-      const speed = 600;
-      let href = $(this).attr("href");
-      let target = $(href == "#" || href == "" ? "html" : href);
-      // ヘッダーの高さ分下げる
-      let position = target.offset().top - headerHeight;
-      $("body,html").animate({ scrollTop: position }, speed, "swing");
-      return false;
-    });
+  // ヘッダーの高さ取得
+  const headerHeight = $(".js-header").height();
+  $('a[href^="#"]').click(function () {
+    const speed = 600;
+    let href = $(this).attr("href");
+    let target = $(href == "#" || href == "" ? "html" : href);
+    // ヘッダーの高さ分下げる
+    let position = target.offset().top - headerHeight;
+    $("body,html").animate({ scrollTop: position }, speed, "swing");
+    return false;
   });
 });
 
